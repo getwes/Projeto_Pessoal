12661e553e1ae0602e275ab396d81ed1fca14b2c
gitwes <wesleyfreire1707@gmail.com>
2024-05-21 21:27:54 -0300

    Atualizaçoes

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-#from main2 import test
+#Importando bibliotecas
+import openpyxl
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from main2 import test
 from main3 import dados
 
-if dados == '1,000':
-    print(dados)
-else:
-    print('wesley deu certo n')
+#criando a planilha
+workbook = openpyxl.Workbook()
+#criando a pagina 'produtos'
+workbook.create_sheet('produtos')
+#selecionando a pagina produtos
+sheet_produtos = workbook['produtos']
+sheet_produtos['A1'].value = 'produto'
+sheet_produtos['B1'].value = 'preço'
+# inserir os titulos e preços na planilha ##zip para que se um titulo não tiver um preço não é atribuido
+for total in zip(test,dados):
+    sheet_produtos.append([total.text])
+
+#salvar
+workbook.save('produtos.xlsx')
+
+
+
+
+
+#if dados == '1,000':
+    #print(dados)
+#else:
+    #print('wesley deu certo n')
