fa784ba1f1faf8e18c23e54773f2512154dbbc4f
gitwes <wesleyfreire1707@gmail.com>
2024-05-22 22:07:25 -0300

    Atualizaçoes

--- arquivos/main.py
+++ arquivos/main.py
@@ -1,9 +1,19 @@
 import os
 import pandas as pd #pip install pandas
 
-import main2
-import main3
-print(main2,main3)
+#import main2
+#import main3
+#print(main2,main3)
 
-data_arquivo_floder = 'arquivos'
+data_arquivo_folder = 'arquivos/'
 
+df = []
+
+for file in os.listdir(data_arquivo_folder):
+    if file.endswith('.xlsx'):
+        print('loading file{}...'.format(file))
+        df.append(pd.read_excel(os.path.join(data_arquivo_folder, file)))
+print(len(df))
+
+df_principal = pd.concat(df, axis=0)
+df_principal.to_excel('arquivos/master_store.xlsx', index=False)
